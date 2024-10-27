1af9fd43ae44958ec6255306178f9912581a2e79
Jason Goemaat <jason.goemaat@gmail.com>
2024-10-26 21:44:44 -0500

    Simple Server

--- README.md
+++ README.md
@@ -11,3 +11,10 @@ Initialized a go mod with this:
     go mod init github.com/JasonGoemaat/go-spa
 
 Wrote `main.go` file to print 'Hello, World!' to console.
+
+## Simple Server
+
+Listen on localhost port 7000 and serve 'Hello, World!'
+to requests.
+
+
--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, World!"))
+	})
+	fmt.Println("Listening on localhost port 7000")
+	http.ListenAndServe("localhost:7000", nil)
 }
